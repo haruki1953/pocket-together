7c03104def0c2bfc85e16146400971faf93585c2
haruki <kinikun@outlook.com>
2025-08-09 12:12:29 +0800

    封装 CropDialog

--- vue3/src/views/setting/views/SettingProfile.vue/components/UpdateAvatar.vue/components/CropDialog.vue
+++ vue3/src/views/setting/views/SettingProfile.vue/components/UpdateAvatar.vue/components/CropDialog.vue
@@ -1,7 +1,161 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { fileUserAvatarConfig } from '@/config'
+import { useI18nStore } from '@/stores'
+import { imageLoadImageFromBlobService, imageResizeImageService } from '@/utils'
+// 图片裁剪依赖
+import { Cropper } from 'vue-advanced-cropper'
+import 'vue-advanced-cropper/dist/style.css'
+
+const i18nStore = useI18nStore()
+
+const originalImage = defineModel<string | null>('originalImage', {
+  required: true,
+})
+const imageUrlRef = defineModel<string | null>('imageUrlRef', {
+  required: true,
+})
+const imageBlobRef = defineModel<Blob | null>('imageBlobRef', {
+  required: true,
+})
+
+const cropDialogVisible = ref(false)
+const open = () => {
+  cropDialogVisible.value = true
+}
+const close = () => {
+  cropDialogVisible.value = false
+}
+
+defineExpose({
+  open,
+  close,
+})
+
+// 引用 vue-advanced-cropper 组件
+const cropperRef = ref<InstanceType<typeof Cropper> | null>(null)
+
+// 成功拿到裁剪好又转化成了 Blob 的图片后
+// 对 Blob 进行调整 尺寸、压缩 验证大小 更新预览
+const crop = () => {
+  if (cropperRef.value != null) {
+    const result = cropperRef.value.getResult()
+    result.canvas?.toBlob(
+      async (blob) => {
+        if (blob != null) {
+          const imageBlob = await (async () => {
+            // 使用函数11将裁剪的 Blob 加载成 url 存到 imageEl
+            const imageEl = await imageLoadImageFromBlobService(blob)
+            // 将图像尺寸统一调整为预设值q
+            const imageResize = imageResizeImageService(
+              imageEl,
+              fileUserAvatarConfig.imageResizeNumber,
+              fileUserAvatarConfig.imageResizeNumber
+            )
+            const imageBlob = await new Promise<Blob>((resolve) => {
+              imageResize.toBlob(
+                (blob) => {
+                  if (!blob) {
+                    throw new Error()
+                  }
+                  resolve(blob)
+                },
+                fileUserAvatarConfig.toBlobType,
+                fileUserAvatarConfig.toBlobQuality
+              )
+            })
+            return imageBlob
+          })()
+
+          // 验证处理后 Blob 大小
+          ;(() => {
+            if (imageBlob.size >= fileUserAvatarConfig.imageBlobSizeNotGte) {
+              throw new Error('imageBlob.size >= 100000')
+            }
+          })()
+          // 释放旧 URL 对象
+          if (imageUrlRef.value != null) {
+            URL.revokeObjectURL(imageUrlRef.value)
+          }
+          imageBlobRef.value = imageBlob
+          // 在这一步为前面的 裁剪后图片的预览 URL 赋值
+          imageUrlRef.value = URL.createObjectURL(imageBlob)
+        }
+        // 关闭
+        cropDialogVisible.value = false
+      },
+      fileUserAvatarConfig.toBlobType,
+      fileUserAvatarConfig.toBlobQuality
+    )
+  }
+}
+</script>
 
 <template>
-  <div></div>
+  <div class="crop-dialog">
+    <!-- 裁剪对话框 -->
+    <ElDialog
+      v-model="cropDialogVisible"
+      :title="i18nStore.t('settingProfileUpdateAvatarDialogTitle')()"
+      width="80%"
+    >
+      <div class="flow-root rounded-t-3xl bg-color-background-soft">
+        <!-- title -->
+        <div class="my-0 hidden xl:my-4 xl:block">
+          <span class="m-8 text-xl font-bold text-color-text-soft">{{
+            i18nStore.t('settingProfileUpdateAvatarDialogTitle')()
+          }}</span>
+        </div>
+        <div class="mx-0 xl:mx-8">
+          <div
+            class="relative flex h-[600px] w-full justify-center overflow-hidden rounded-t-3xl xl:h-[400px] xl:rounded-3xl"
+          >
+            <img
+              v-if="originalImage"
+              :src="originalImage"
+              class="absolute left-0 top-0 h-full w-full scale-110 object-cover opacity-40 blur-lg"
+            />
+            <Cropper
+              v-if="originalImage"
+              ref="cropperRef"
+              :src="originalImage"
+              :stencilProps="{
+                aspectRatio: 1,
+              }"
+              class="cropperBg"
+            />
+          </div>
+          <!-- 占位 div -->
+          <div class="h-[2px] w-full"></div>
+        </div>
+      </div>
+      <!-- 再复制一份样式创建按钮 -->
+      <div
+        class="mt-[2px] flow-root rounded-b-3xl bg-color-background-soft xl:mt-0"
+      >
+        <div
+          class="poto-setting-button-box not-center mb-4 mr-2 mt-2 xl:mr-8 xl:mt-4"
+        >
+          <span class="dialog-footer">
+            <ElButton round @click="cropDialogVisible = false">{{
+              i18nStore.t('settingProfileUpdateAvatarDialogCancelButton')()
+            }}</ElButton>
+            <ElButton type="primary" round @click="crop">{{
+              i18nStore.t('settingProfileUpdateAvatarDialogCropButton')()
+            }}</ElButton>
+          </span>
+        </div>
+      </div>
+    </ElDialog>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.cropperBg {
+  :deep() {
+    .vue-advanced-cropper__background,
+    .vue-advanced-cropper__wrapper {
+      background-color: unset;
+    }
+  }
+}
+</style>
--- vue3/src/views/setting/views/SettingProfile.vue/components/UpdateAvatar.vue/index.vue
+++ vue3/src/views/setting/views/SettingProfile.vue/components/UpdateAvatar.vue/index.vue
@@ -12,15 +12,11 @@ import {
   blobToFile,
   compareDatesSafe,
   fetchWithTimeoutPreferred,
-  imageLoadImageFromBlobService,
-  imageResizeImageService,
   potoMessage,
 } from '@/utils'
 import { useMutation, useQueryClient } from '@tanstack/vue-query'
 import type { UploadFile } from 'element-plus'
-// 图片裁剪依赖
-import { Cropper } from 'vue-advanced-cropper'
-import 'vue-advanced-cropper/dist/style.css'
+import CropDialog from './components/CropDialog.vue'
 
 const i18nStore = useI18nStore()
 
@@ -52,74 +48,20 @@ const imageForShow = computed(() => {
   return profileAvatarUrl.value
 })
 
-// 引用 vue-advanced-cropper 组件
-const cropperRef = ref<InstanceType<typeof Cropper> | null>(null)
 // 对话框显示隐藏
-const cropDialogVisible = ref(false)
+// const cropDialogVisible = ref(false)
 // 存储用户选择的原始图片的临时 URL，用于在裁剪器中显示
 const originalImage = ref<string | null>(null)
+
+// 引用CropDialog
+const refCropDialog = ref<InstanceType<typeof CropDialog> | null>(null)
 
 // 当用户通过 ElUpload 选择图片后触发
 // 用 URL.createObjectURL() 创建 原始图片 的临时 URL，并显示裁剪对话框
 const onImageSelect = (uploadFile: UploadFile) => {
   if (uploadFile.raw) {
     originalImage.value = URL.createObjectURL(uploadFile.raw)
-    cropDialogVisible.value = true
-  }
-}
-
-// 成功拿到裁剪好又转化成了 Blob 的图片后
-// 对 Blob 进行调整 尺寸、压缩 验证大小 更新预览
-const crop = () => {
-  if (cropperRef.value != null) {
-    const result = cropperRef.value.getResult()
-    result.canvas?.toBlob(
-      async (blob) => {
-        if (blob != null) {
-          const imageBlob = await (async () => {
-            // 使用函数11将裁剪的 Blob 加载成 url 存到 imageEl
-            const imageEl = await imageLoadImageFromBlobService(blob)
-            // 将图像尺寸统一调整为预设值q
-            const imageResize = imageResizeImageService(
-              imageEl,
-              fileUserAvatarConfig.imageResizeNumber,
-              fileUserAvatarConfig.imageResizeNumber
-            )
-            const imageBlob = await new Promise<Blob>((resolve) => {
-              imageResize.toBlob(
-                (blob) => {
-                  if (!blob) {
-                    throw new Error()
-                  }
-                  resolve(blob)
-                },
-                fileUserAvatarConfig.toBlobType,
-                fileUserAvatarConfig.toBlobQuality
-              )
-            })
-            return imageBlob
-          })()
-
-          // 验证处理后 Blob 大小
-          ;(() => {
-            if (imageBlob.size >= fileUserAvatarConfig.imageBlobSizeNotGte) {
-              throw new Error('imageBlob.size >= 100000')
-            }
-          })()
-          // 释放旧 URL 对象
-          if (imageUrlRef.value != null) {
-            URL.revokeObjectURL(imageUrlRef.value)
-          }
-          imageBlobRef.value = imageBlob
-          // 在这一步为前面的 裁剪后图片的预览 URL 赋值
-          imageUrlRef.value = URL.createObjectURL(imageBlob)
-        }
-        // 关闭
-        cropDialogVisible.value = false
-      },
-      fileUserAvatarConfig.toBlobType,
-      fileUserAvatarConfig.toBlobQuality
-    )
+    refCropDialog.value?.open()
   }
 }
 
@@ -283,59 +225,12 @@ const submit = mutation.mutateAsync
     </div>
 
     <!-- 裁剪对话框 -->
-    <ElDialog
-      v-model="cropDialogVisible"
-      :title="i18nStore.t('settingProfileUpdateAvatarDialogTitle')()"
-      width="80%"
-    >
-      <div class="flow-root rounded-t-3xl bg-color-background-soft">
-        <!-- title -->
-        <div class="my-0 hidden xl:my-4 xl:block">
-          <span class="m-8 text-xl font-bold text-color-text-soft">{{
-            i18nStore.t('settingProfileUpdateAvatarDialogTitle')()
-          }}</span>
-        </div>
-        <div class="mx-0 xl:mx-8">
-          <div
-            class="relative flex h-[600px] w-full justify-center overflow-hidden rounded-t-3xl xl:h-[400px] xl:rounded-3xl"
-          >
-            <img
-              v-if="originalImage"
-              :src="originalImage"
-              class="absolute left-0 top-0 h-full w-full scale-110 object-cover opacity-40 blur-lg"
-            />
-            <Cropper
-              v-if="originalImage"
-              ref="cropperRef"
-              :src="originalImage"
-              :stencilProps="{
-                aspectRatio: 1,
-              }"
-              class="cropperBg"
-            />
-          </div>
-          <!-- 占位 div -->
-          <div class="h-[2px] w-full"></div>
-        </div>
-      </div>
-      <!-- 再复制一份样式创建按钮 -->
-      <div
-        class="mt-[2px] flow-root rounded-b-3xl bg-color-background-soft xl:mt-0"
-      >
-        <div
-          class="poto-setting-button-box not-center mb-4 mr-2 mt-2 xl:mr-8 xl:mt-4"
-        >
-          <span class="dialog-footer">
-            <ElButton round @click="cropDialogVisible = false">{{
-              i18nStore.t('settingProfileUpdateAvatarDialogCancelButton')()
-            }}</ElButton>
-            <ElButton type="primary" round @click="crop">{{
-              i18nStore.t('settingProfileUpdateAvatarDialogCropButton')()
-            }}</ElButton>
-          </span>
-        </div>
-      </div>
-    </ElDialog>
+    <CropDialog
+      ref="refCropDialog"
+      v-model:originalImage="originalImage"
+      v-model:imageUrlRef="imageUrlRef"
+      v-model:imageBlobRef="imageBlobRef"
+    ></CropDialog>
   </div>
 </template>
 
