a5242f467ae84ad24d36e7c6eb455c3486d086db
haruki <kinikun@outlook.com>
2025-08-11 16:54:45 +0800

    fix(project): 完善文档，顺便完善了pb后端

---
+++ pocketbase/pb_migrations/1754892210_updated_messages.js
@@ -0,0 +1,20 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2605467279")
+
+  // update collection data
+  unmarshal({
+    "createRule": "// 创建消息需登录\n@request.auth.id != \"\" &&\n// 创建时需为创建者（传入的author需为当前用户）\n@request.auth.id = author.id"
+  }, collection)
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2605467279")
+
+  // update collection data
+  unmarshal({
+    "createRule": "// 创建消息需登录\n@request.auth.id != \"\""
+  }, collection)
+
+  return app.save(collection)
+})
