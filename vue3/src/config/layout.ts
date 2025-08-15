c762215e353e99b405fabfc3c3346010732eda7c
haruki <kinikun@outlook.com>
2025-08-15 16:30:37 +0800

    feat(chat): 聊天页响应式

--- vue3/src/config/layout.ts
+++ vue3/src/config/layout.ts
@@ -5,7 +5,7 @@ export const layoutLayoutNavConfig = {
   /**
    * 导航栏布局配置
    * rightNav >= 768 > botttomNav |
-   * 大于768将显示右侧导航栏，小于768将显示底部导航栏
+   * 大于等于768将显示右侧导航栏，小于768将显示底部导航栏
    * useWindowSize
    */
   breakpointNavRightToNavBottom: 768,
@@ -21,7 +21,7 @@ export const layoutLayoutNavConfig = {
 export const layoutLoginPageConfig = {
   /**
    * Col2 >= 768 > Col1
-   * 大于768显示两列，小于则显示一列
+   * 大于等于768显示两列，小于则显示一列
    * useWindowSize
    */
   breakpointCol2ToCol1: 768,
@@ -33,7 +33,7 @@ export const layoutLoginPageConfig = {
 export const layoutSettingPageConfig = {
   /**
    * leftNav >= 768 > topNav |
-   * 大于768将显示左侧导航栏，小于768将显示顶部导航栏
+   * 大于等于768将显示左侧导航栏，小于768将显示顶部导航栏
    * 和 全局导航栏 类似
    * useWindowSize
    */
@@ -49,6 +49,26 @@ export const layoutSettingPageConfig = {
    */
   breakpointContentCol2ToCol1: 1024,
 } as const
+
+/** 全局聊天页布局配置 */
+export const layoutChatPageConfig = {
+  /**
+   * Col2 >= 768 > Col1
+   * 大于等于768显示两列，小于则显示一列（只显示用户列表，聊天在另外的页面）
+   * useWindowSize
+   */
+  breakpointCol2ToCol1: 768,
+  /**
+   * Width500 >= 1024 > Width400
+   * 大于1024时聊天栏宽度为500，小于则为400
+   * useWindowSize
+   */
+  breakpointChatWidth5ToWidth4: 1024,
+  /**
+   * 导航栏位于底部时（小屏），内容的最大宽度
+   */
+  contentMaxWidthOnNavBottom: 500,
+}
 
 // 参考
 // breakpoints: {
--- vue3/src/views/chat/ChatPage.vue/index.vue
+++ vue3/src/views/chat/ChatPage.vue/index.vue
@@ -1,20 +1,46 @@
 <script setup lang="ts">
 import { useI18nStore } from '@/stores'
 import ChatInputBar from './components/ChatInputBar.vue'
+import { useWindowSize } from '@vueuse/core'
+import { layoutChatPageConfig } from '@/config'
 
 const i18nStore = useI18nStore()
 useSeoMeta({
   title: computed(() => i18nStore.t('pageChat')()),
 })
+
+const { width: windowWidth } = useWindowSize()
+/** 大于等于768显示两列，小于则显示一列（只显示用户列表，聊天在另外的页面） */
+const showCol2TrueCol1False = computed(() => {
+  if (windowWidth.value >= layoutChatPageConfig.breakpointCol2ToCol1) {
+    return true
+  }
+  return false
+})
+/** 大于1024时聊天栏宽度为500，小于则为400 */
+const showChatWidth5TrueWidth4False = computed(() => {
+  if (windowWidth.value >= layoutChatPageConfig.breakpointChatWidth5ToWidth4) {
+    return true
+  }
+  return false
+})
 </script>
 
 <template>
   <div class="chat-page">
-    <div class="container-col2-box">
+    <div v-if="showCol2TrueCol1False" class="container-col2-box">
       <ContainerCol2
         col1Position="right"
         col1Twcss="flex-1"
-        col2Twcss="w-[500px]"
+        :col2Twcss="
+          (() => {
+            /** 大于1024时聊天栏宽度为500，小于则为400 */
+            if (showChatWidth5TrueWidth4False) {
+              return 'w-[500px]'
+            }
+            return 'w-[400px]'
+          })()
+        "
       >
         <template #col1>
           <div class="my-6 ml-2">
@@ -42,6 +68,23 @@ useSeoMeta({
           </div>
         </template>
       </ContainerCol2>
+    </div>
+    <div v-else class="container-col1-box">
+      <div class="mx-2 mt-6">
+        <div
+          class="mx-auto"
+          :style="{
+            maxWidth: `${layoutChatPageConfig.contentMaxWidthOnNavBottom}px`,
+          }"
+        >
+          <div>
+            <div>小屏全局聊天页跳转</div>
+            <div>用户列表</div>
+            <div class="h-[2000px] bg-red-950"></div>
+            <div>用户列表</div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
