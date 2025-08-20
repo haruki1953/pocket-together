88cf1908b228c02750a750daf4df1d872d17d794
切符 <KIPPUDESU@OUTLOOK.com>
2025-08-20 22:35:05 +0800

    feat: Home page UI adjustment
    - 调整创建房间按钮的UI
    - 设计图改名

--- vue3/src/views/home/HomeMenu.vue
+++ vue3/src/views/home/HomeMenu.vue
@@ -1,26 +1,27 @@
 <script setup lang="ts"></script>
 
 <template>
-  <RouterLink
-    :to="{ name: 'CreateRoomPage' }"
+  <div
     class="group mb-4 transform-gpu break-inside-avoid overflow-hidden rounded-2xl bg-color-background-soft p-4 shadow-lg transition-all duration-300 ease-in-out hover:!opacity-100"
   >
-    <!-- 添加房间 -->
-    <div
-      class="group transform-gpu cursor-pointer break-inside-avoid rounded-2xl border-2 border-dashed border-gray-300 p-6 transition-all hover:border-blue-400 hover:bg-gray-100 hover:shadow-xl dark:border-gray-700 dark:hover:border-blue-500 dark:hover:bg-gray-800/50 sm:mb-6"
-    >
-      <div class="flex flex-col items-center justify-center">
-        <div
-          class="text-4xl text-gray-400 transition-colors group-hover:text-blue-500"
-        >
-          +
-        </div>
-        <div
-          class="mt-2 text-sm font-medium text-gray-500 transition-colors group-hover:text-blue-500 dark:text-gray-400"
-        >
-          创建新房间
+    <RouterLink :to="{ name: 'CreateRoomPage' }">
+      <!-- 添加房间 -->
+      <div
+        class="group transform-gpu cursor-pointer break-inside-avoid rounded-2xl border-2 border-dashed border-gray-300 p-6 transition-all hover:border-blue-400 hover:bg-gray-100 hover:shadow-xl dark:border-gray-700 dark:hover:border-blue-500 dark:hover:bg-gray-800/50"
+      >
+        <div class="flex flex-col items-center justify-center">
+          <div
+            class="text-4xl text-gray-400 transition-colors group-hover:text-blue-500"
+          >
+            +
+          </div>
+          <div
+            class="mt-2 text-sm font-medium text-gray-500 transition-colors group-hover:text-blue-500 dark:text-gray-400"
+          >
+            创建新房间
+          </div>
         </div>
       </div>
-    </div>
-  </RouterLink>
+    </RouterLink>
+  </div>
 </template>
