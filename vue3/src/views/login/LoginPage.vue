2a70d5ec4339697e43cf7676c60149cdd2f884f1
haruki <kinikun@outlook.com>
2025-10-01 19:10:09 +0800

    chore(login): 优化了登录页面的显示

--- vue3/src/views/login/LoginPage.vue
+++ vue3/src/views/login/LoginPage.vue
@@ -26,101 +26,112 @@ const showCol2TrueCol1False = computed(() => {
   }
   return false
 })
+
+const isMounted = ref(false)
+onMounted(() => {
+  isMounted.value = true
+})
 </script>
 
 <template>
   <div>
-    <div class="flex min-h-screen items-center justify-center">
-      <div class="w-full max-w-[1000px]">
-        <div
-          :class="{
-            'mx-10': showCol2TrueCol1False,
-            'mx-2': !showCol2TrueCol1False,
-          }"
-        >
+    <!-- isMounted实现出现时过渡动画 -->
+    <Transition name="fade">
+      <div
+        v-if="isMounted"
+        class="flex min-h-screen items-center justify-center"
+      >
+        <div class="w-full max-w-[1000px]">
           <div
             :class="{
-              'text-5xl': showCol2TrueCol1False,
-              'text-3xl': !showCol2TrueCol1False,
+              'mx-10': showCol2TrueCol1False,
+              'mx-2': !showCol2TrueCol1False,
             }"
-            class="gradient-text wrap-long-text select-none py-10 text-center font-bold"
           >
-            {{ i18nStore.t('appNameI18n')() }}
-          </div>
-          <!-- 两列 -->
-          <div
-            v-if="showCol2TrueCol1False"
-            class="flex items-center justify-center"
-          >
-            <!-- 左栏 -->
             <div
-              ref="refBoxLeft"
-              :style="styleBoxLeftRight.left"
-              class="flex-1 rounded-3xl bg-color-background-soft"
+              :class="{
+                'text-5xl': showCol2TrueCol1False,
+                'text-3xl': !showCol2TrueCol1False,
+              }"
+              class="gradient-text wrap-long-text select-none py-10 text-center font-bold"
             >
-              <!-- 左侧上栏 -->
-              <div class="mx-auto max-w-96">
-                <div class="m-8">
-                  <LoginForm></LoginForm>
-                </div>
-              </div>
-              <!-- 左侧水平分割线 -->
-              <!-- 左侧下栏 -->
-              <!-- 都在Oauth2List -->
-              <Oauth2List></Oauth2List>
+              {{ i18nStore.t('appNameI18n')() }}
             </div>
-            <!-- 中间垂直分割线 -->
-            <div class="w-0 border border-transparent"></div>
-            <!-- 右栏 -->
+            <!-- 两列 -->
             <div
-              ref="refBoxRight"
-              :style="styleBoxLeftRight.right"
-              class="flex-1 rounded-3xl bg-color-background-soft"
+              v-if="showCol2TrueCol1False"
+              class="flex items-center justify-center"
             >
-              <div class="mx-auto max-w-96">
-                <div class="m-8">
-                  <RegisterForm></RegisterForm>
+              <!-- 左栏 -->
+              <div
+                ref="refBoxLeft"
+                :style="styleBoxLeftRight.left"
+                class="flex-1 rounded-3xl bg-color-background-soft"
+              >
+                <!-- 左侧上栏 -->
+                <div class="mx-auto max-w-96">
+                  <div class="m-8">
+                    <LoginForm></LoginForm>
+                  </div>
+                </div>
+                <!-- 左侧水平分割线 -->
+                <!-- 左侧下栏 -->
+                <!-- 都在Oauth2List -->
+                <Oauth2List></Oauth2List>
+              </div>
+              <!-- 中间垂直分割线 -->
+              <div class="w-0 border border-transparent"></div>
+              <!-- 右栏 -->
+              <div
+                ref="refBoxRight"
+                :style="styleBoxLeftRight.right"
+                class="flex-1 rounded-3xl bg-color-background-soft"
+              >
+                <div class="mx-auto max-w-96">
+                  <div class="m-8">
+                    <RegisterForm></RegisterForm>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <!-- 一列 -->
-          <div v-else class="flex items-center justify-center">
-            <div
-              class="w-full max-w-[400px] rounded-3xl bg-color-background-soft"
-            >
-              <div class="mx-auto max-w-96">
-                <div class="m-8">
-                  <LoginForm></LoginForm>
+            <!-- 一列 -->
+            <div v-else class="flex items-center justify-center">
+              <div
+                class="w-full max-w-[400px] rounded-3xl bg-color-background-soft"
+              >
+                <div class="mx-auto max-w-96">
+                  <div class="m-8">
+                    <LoginForm></LoginForm>
+                  </div>
                 </div>
-              </div>
-              <Oauth2List></Oauth2List>
-              <div class="border border-color-background"></div>
-              <div class="mx-auto max-w-96">
-                <div class="m-8">
-                  <RegisterForm></RegisterForm>
+                <Oauth2List></Oauth2List>
+                <div class="border border-color-background"></div>
+                <div class="mx-auto max-w-96">
+                  <div class="m-8">
+                    <RegisterForm></RegisterForm>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div class="m-10 flex items-center justify-center">
-            <LoginDarkI18n></LoginDarkI18n>
-            <div class="mx-1 cursor-pointer p-1">
-              <i
-                class="ri-discord-line"
-                style="font-size: 24px; line-height: 24px"
-              ></i>
-            </div>
-            <div class="mx-1 cursor-pointer p-1">
-              <i
-                class="ri-github-line"
-                style="font-size: 24px; line-height: 24px"
-              ></i>
+            <div class="m-10 flex items-center justify-center">
+              <LoginDarkI18n></LoginDarkI18n>
+              <div class="mx-1 cursor-pointer p-1">
+                <i
+                  class="ri-discord-line"
+                  style="font-size: 24px; line-height: 24px"
+                ></i>
+              </div>
+              <div class="mx-1 cursor-pointer p-1">
+                <i
+                  class="ri-github-line"
+                  style="font-size: 24px; line-height: 24px"
+                ></i>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Transition>
   </div>
 </template>
 
